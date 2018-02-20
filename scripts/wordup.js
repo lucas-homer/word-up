742bf5f830b0f3a6245801edddc98ad18177a12a
Lucas Homer <lucas.homer@gmail.com>
2018-02-20 11:07:40 -0600

    implement ToDo3 - focus the textbox

--- scripts/wordup.js
+++ scripts/wordup.js
@@ -147,9 +147,9 @@ function render() {
 
     // Set the value of the textbox
     $("#textbox").val(model.currentAttempt);
-    // TODO 3
+    
     // Give focus to the textbox.
-
+    $("#textbox").focus();
 
     // if the current word attempt contains disallowed letters,
     var disallowedLetters = disallowedLettersInWord(model.currentAttempt);
